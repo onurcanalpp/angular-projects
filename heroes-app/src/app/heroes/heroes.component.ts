f1b9b72ce3158510a96753d35f0bbbe84ce345ed
onurcanalpp <onurcanalpp@gmail.com>
2021-11-25 19:32:38 +0300

    getting hero details and controlling

--- heroes-app/src/app/heroes/heroes.component.ts
+++ heroes-app/src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
@@ -8,7 +10,15 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+    console.log(this.selectedHero);
+  }
+
 
   hero: Hero = {
     id: 1,
@@ -18,7 +28,7 @@ export class HeroesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    
+    console.log();
   }
 
 }
---
+++ heroes-app/src/app/mock-heroes.ts
@@ -0,0 +1,36 @@
+import { Hero } from './hero';
+
+export const  HEROES: Hero[] = [
+  {
+    id: 1,
+    name: 'Dr Nice'
+  },
+  {
+    id: 2,
+    name: 'Narco'
+  },
+  {
+    id: 3,
+    name: 'Bombasto'
+  },
+  {
+    id: 4,
+    name: 'Celeritas'
+  },
+  {
+    id: 5,
+    name: 'Magneta'
+  },
+  {
+    id: 6,
+    name: 'RubberMan'
+  },
+  {
+    id: 7,
+    name: 'Dynama'
+  },
+  {
+    id: 8,
+    name: 'Dr IQ'
+  }
+]
